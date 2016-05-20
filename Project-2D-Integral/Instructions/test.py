5edf4e9bd93c51b605025a8723e54e55c1783a4b
Rickard Carlsson <nat15rca@student.lu.se>
2016-05-20 09:07:09 +0200

    shitty code

--- Project-2D-Integral/Instructions/test.py
+++ Project-2D-Integral/Instructions/test.py
@@ -4,20 +4,23 @@ from pylab import *
 
 
 dataMatrix1 = genfromtxt('coord1_1.txt')
-c1 = dataMatrix1[:1].astype(int)
-c2 = dataMatrix1[1:2,].astype(int)
+c1 = dataMatrix1[:1]
+c2 = dataMatrix1[1:2,]
 
 dataMatrix2 = genfromtxt('elementnode1_1.txt')
 e1= dataMatrix2[:1].astype(int)
 e2 = dataMatrix2[1:2,].astype(int)
 e3= dataMatrix2[2:3,].astype(int)
-print (c1[e1[1]], c1[e1[1]])
 
-#elements = []
-#for i in range(0, len(dataMatrix2)):
-#    elements = np.append([elements,  c1[e1[i]], c2[e1[i]], c1[e2[i]], c2[e2[i]], c2[e3[i]], c2[e3[i]] ] )
-#    print (elements)
-#print (elements)
+print (len(c1[0]))
+x = []
+y = []
+for i in range(0, len(e1[0])):
+	x.append([ [ c1[0][e1[0][i]], c1[0][e2[0][i]], c1[0][e3[0][i]] ] ])
+	y.append([ [ c2[0][e1[0][i]], c2[0][e2[0][i]], c2[0][e3[0][i]] ] ])
+print (x)
+print (y) 
 
-plt.plot(x, y, '*')
-plt.show()
+#plt.plot(x, y, '*')
+#plt.show()
+
