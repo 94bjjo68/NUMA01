93390086b0a184c539a9f9b4c759c94e243b5df4
Rickard Carlsson <nat15rca@student.lu.se>
2016-05-22 16:01:38 +0200

    Final_project

--- Project-2D-Integral/Instructions/test.py
+++ Project-2D-Integral/Instructions/test.py
@@ -1,6 +1,6 @@
 import numpy as np
 from pylab import *
-
+from random import randint
 
 
 dataMatrix1 = genfromtxt('coord1_1.txt')
@@ -15,12 +15,15 @@ e3= dataMatrix2[2:3,].astype(int)
 print (len(c1[0]))
 x = []
 y = []
-for i in range(0, len(e1[0])):
-	x.append([ [ c1[0][e1[0][i]], c1[0][e2[0][i]], c1[0][e3[0][i]] ] ])
-	y.append([ [ c2[0][e1[0][i]], c2[0][e2[0][i]], c2[0][e3[0][i]] ] ])
-print (x)
-print (y) 
-
-#plt.plot(x, y, '*')
-#plt.show()
+#for i in range(0, len(e1[0])):
+#	x.append([ [ c1[0][e1[0][i]], c1[0][e2[0][i]], c1[0][e3[0][i]] ] ])
+#	y.append([ [ c2[0][e1[0][i]], c2[0][e2[0][i]], c2[0][e3[0][i]] ] ])
+print ( [ c2[0][e1[0][20]], c2[0][e2[0][20]], c2[0][e3[0][20]] ] )
+print (e1[0]) 
+print (e2[0])
+print (e3[0])
+col=['green', 'red', 'blue']
+for i in range(0, len(e1[0])-1):
+	plt.plot([ c1[0][e1[0][i]-1], c1[0][e2[0][i]], c1[0][e3[0][i]-1] ] , [ c2[0][e1[0][i]-1], c2[0][e2[0][i]-1], c2[0][e3[0][i]-1] ],'--k', c=col[randint(0,2)] )
+plt.show()
 
