7373ff199826d600426f90918b09ad8ff947a262
Rickard Carlsson <rickard.carlsson4@gmail.com>
2016-07-19 19:31:31 +0200

    update

--- homework2/homework2.py
+++ homework2/homework2.py
@@ -1,15 +1,34 @@
+import numpy as np
+
 
 class Interval:
-    def __init__(self, A, B):
-        self.a = A 
-	other.b = B 
-def __add__(self ,other):
-	return Interval( self.a + other.b) 
+	def __init__(self, a, b):
+		self.a = a 
+		self.b = b 
+
+	def __repr__(self):
+		return repr([self.a, self.b])
+
+	def __add__(self, other):
+		return Interval( self.a + other.a, self.b + other.b) 
+
+	def __sub__(self, other):
+		return Interval(self.a - other.b, self.b - other.a)
+		 
+	def __mul__(self, other):
+		return Interval(self.a * other.a, self.b * other.b)
+
+	def __truediv__(self, other):
+		if (other.a, other.b) == 0 or np.inf:
+			return 'Please make sure values is of sort int and x !=0' 
+		else:
+			return nterval(self.a / other.b, self.b / other.a)
+
 
 I1 = Interval(1, 2)
-I2 = Interval(2, 4)
-
-print (I1+I2)
+I2 = Interval(1, 4)
+print(I1)
+print(I2 + I1)
 print (I1-I2)
 print (I1*I2)
 print (I1/I2)
