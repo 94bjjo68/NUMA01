7b368360d03f8c273c3f36ff081a0bc1f9f16e6f
posterus <rickard.carlsson4@gmail.com>
2016-08-19 19:45:10 +0200

    task 8

--- homework2/homework2.py
+++ homework2/homework2.py
@@ -13,9 +13,16 @@ class Interval:
 	def __repr__(self):
 		return repr([self.a, self.b])
 
-	def __add__(self, other):
-		return Interval( self.a + other.a, self.b + other.b)
+	def __add__(self, other ): 
+		if(isinstance(self,Interval) and isinstance(other, Interval)):
+			return Interval( self.a + other.a, self.b + other.b)
+		if(isinstance(other,( int, float))):
+			other = Interval(other)
+			return Interval(self.a + other.a, self.b + other.b)
 
+
+	def __radd__(self, other):
+		return Interval(self.a + other, self.b + other)
 	def __sub__(self, other):
 		return Interval(self.a - other.b, self.b - other.a)
 
@@ -31,7 +38,10 @@ class Interval:
 
 I1 = Interval(1, 2)
 I2 = Interval(1, 4)
-
-
+print(I1 + I2) 
+print(I2+ I1)
+print(I2 + 1.0)
+print(1.0 + I2)
+	
 
 print(Interval(1))
