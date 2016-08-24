72f9d12dc5ffb37815cc6a83c9b633a6da377594
posterus <rickard.carlsson4@gmail.com>
2016-08-24 19:59:39 +0200

    done

--- homework2/homework2.py
+++ homework2/homework2.py
@@ -24,30 +24,56 @@ class Interval:
 	def __radd__(self, other):
 		return Interval(self.a + other, self.b + other)
 	def __sub__(self, other):
+		if(isinstance(other, (int, float))):
+			other = Interval(other)
 		return Interval(self.a - other.b, self.b - other.a)
 
-	def __mul__(self, other):
-		return Interval(self.a * other.a, self.b * other.b)
+	def __mul__(self, other):	
+		if(isinstance(other, (int, float))):
+			other = Interval(other)
+		variations = [self.a*other.a, self.a*other.b, self.b*other.b]
+		return Interval(min(variations), max(variations))
+	
+	def __rmul__(self, other):	
+		if(isinstance(other, (int, float))):
+			other = Interval(other)
+		variations = [self.a*other.a, self.a*other.b, self.b*other.b]
+		return Interval(min(variations), max(variations))
 
 	def __truediv__(self, other):
-		if (other.a or other.b) == 0 or np.inf:
-			return 'Please make sure values is of sort int and x !=0'
+		if 0 in Interval(other.a, other.b) or  np.inf:
+			raise ValueError('Please make sure values is of sort int and x !=0')
 		else:
-			return nterval(self.a / other.b, self.b / other.a)
+			variations = [self.a/othe.a , self.a/other.b , self.b/other.a, self.b/other.b]
+			return Interval(min(variations), max(variations))
 	def __pow__(self, other):
-		return Interval(self.a**other, self.b**other)
+		variations = [self.a**other, self.b**other]
+		return Interval(min(variations), max(variations))
+	def __contains__(self, other):
+		if other>= self.a and other<=self.b:
+			return True
+		else:
+			return False
+	def Return(self):
+		return [self.a, self.b]
 
-
-
+		
 def func(X):
 	return (3*X**3)-(2*X**2)+(5*X)-1
+a=1000
 
 xl = np.linspace(0.,1,1000)
 xu = np.linspace(0.,1,1000) + 0.5
 
-plt.plot(xl, func(xl), c='r')
-plt.plot(xl, func(xu), c='b')
-plt.plot(xl, func(xu)-func(xl), c='black')
+y_list = []
+for n in range(a):
+	lst = func(Interval(xl[n], xu[n])) 
+	y_list.append(lst.Return()) 
+
+
+plt.plot(xl, y_list, c='r')
+
+
 plt.show()
 
 
